696bad27167087106180bbdd2cabd34e80be2932
AbhiKalpita <109658894+AbhiKalpita@users.noreply.github.com>
2022-08-05 09:13:21 +0530

    Add files via upload
    multi commit

---
+++ Multiple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Basic_of_Oops
+{
+    internal class MultipleIn
+{
+    void show()
+    {
+        Console.WriteLine("1 st class Method");
+    }
+}
+}
+
+class Second
+{
+    void show()
+    {
+        void show()
+        {
+            Console.WriteLine("2nd Class Method");
+        }
+    }
+}
+
+class MultipleTest : MultipleIn
+{
